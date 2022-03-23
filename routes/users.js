900229bca1f7b47046b9545c8f73066997646cb7
ArjunSingh814260 <97024803+ArjunSingh814260@users.noreply.github.com>
2022-03-23 17:18:00 +0530

    "added friend Reques API"

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 router.post("/signup", userController.signUp);
+router.post("/friendRequest", userController.friendRequest);
 router.post("/login", AuthController.userlogin);
 
 module.exports = router;
